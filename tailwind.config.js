edd0cb6d63eca31c85c76a7fb786afe01a739138
Adam Fields <me@aef.me>
2024-03-29 11:17:00 -0400

    Add `shadcn-ui`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,31 @@
-import containers from '@tailwindcss/container-queries'
-import forms from '@tailwindcss/forms'
 import typography from '@tailwindcss/typography'
+import animate from 'tailwindcss-animate'
 
 /** @type {import('tailwindcss').Config} */
 const config = {
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{ts,tsx}'],
-  plugins: [containers, forms, typography],
+  plugins: [animate, typography],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    },
     extend: {
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' }
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' }
+        }
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
+      },
       typography: (_) => ({
         DEFAULT: {
           css: {
