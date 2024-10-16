840a712f6ca18e80122b24c0bc79834b0f592fa0
Adam Fields <me@aef.me>
2024-10-15 21:43:57 -0400

    Move to ts-fns repo

--- src/lib/sleep.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Sleep for a given number of milliseconds.
- * @example await sleep(1000)
- */
-export default function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
